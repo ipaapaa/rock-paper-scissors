10bfcedb5659c450438c0bb5baf65cc349915192
Marc Olata <marc.olata123@gmail.com>
2024-08-29 17:20:12 +0800

    Created the getPlayerChoice
    It returns a string based on what the player chooses

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function getCompChoice(){
    
 }
 
+function getPlayerChoice(){
+    
+    console.log("[1] Rock")
+    console.log("[2] Paper")
+    console.log("[3] Scissors")
+    
+    let playerChoice = prompt("Enter your choice [number]:")
+    
+    
+    switch (playerChoice){
+        case 0:
+            return 'Rock'
+        case 1:
+            return 'Paper'
+        case 2:
+            return 'Scissors'
+        default:
+            console.log("Invalid input, please try again.")
+
+    }
+
+}
+
