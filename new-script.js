2d4f9373b5c6259d988f8188a1daa599d1b3215b
Marc Olata <marc.olata123@gmail.com>
2024-09-08 20:17:18 +0800

    Refactored getCompChoice
    transferred it into new-script

--- new-script.js
+++ new-script.js
@@ -1,8 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+function getCompChoice(){
+
+    let compChoice = Math.floor(Math.random()*3)
+    switch(compChoice){
+        case 0: 
+            return 'Rock';
+                
+        case 1: 
+            return 'Paper';
+                
+        case 2: 
+            return 'Scissors';
+                
+        }
+       
+    }
+
 function getPlayerChoice(){
     let playerChoice;
-
     const rock = document.querySelector(".rock");
     rock.addEventListener("click",() => {
         return "Rock"; //for checking
